96a46bf2dd49b914169016ceb2890976fcf7376b
Rahish2009 <57913293+Rahish2009@users.noreply.github.com>
2021-09-12 10:22:22 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('Posenet Is Initalized');
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristX = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +" leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +" rightWristY = " + rightWristY);
+    }
+ }
+
+ function draw(){
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+ }
